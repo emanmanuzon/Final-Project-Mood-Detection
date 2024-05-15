c15e64b3860f1951843d311ab4e7ae6f18421b41
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 01:47:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
-def load_model():
-  model=tf.keras.models.load_model('moodmodel.h5')
-  return model
-model=load_model()
-
 st.write("""
 # Mood Detection"""
 )
