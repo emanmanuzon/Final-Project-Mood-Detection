9f925f4e98feeaf5ea3f509fa44ec4beaae5fc55
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 02:02:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
-  file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
