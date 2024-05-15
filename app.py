1ec458231ea2cc54e264b9824f11a52537349387
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 01:52:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
-st.write("""
-# Mood Detection"""
-)
 file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
