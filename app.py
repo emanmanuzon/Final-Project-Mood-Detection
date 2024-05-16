313df178ab90d0e5cfe4e958df218972c21e18be
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 10:01:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ class VideoTransformer(VideoTransformerBase):
         return img
 
 @st.cache_data(experimental_allow_widgets=True)
+def load_model():
+  model=tf.keras.models.load_model('CNN_Model_7.h5')
+  return model
+model=load_model()
 st.write("""
-# Mood Detection
-""")
+# Weather Detection System"""
+)
 
 webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
