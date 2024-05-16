edfc0d5bd718a959d26aa288c94fce8d05005717
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 09:58:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,6 @@ class VideoTransformer(VideoTransformerBase):
         return img
 
 @st.cache_data(experimental_allow_widgets=True)
-def load_model():
-    model = tf.keras.models.load_model('CNN_Model_7.h5')
-    return model
-
-model = load_model()
-
 st.write("""
 # Mood Detection
 """)
