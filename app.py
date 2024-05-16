46147efe6995cdc573913f1631a85f2e9cd3010b
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 10:26:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ class VideoTransformer(VideoTransformerBase):
             # You can pass the detected faces to your model for mood detection here
         return img
 
-@st.cache_data(experimental_allow_widgets=True)
-def load_model():
-  model=tf.keras.models.load_model('CNN_Model_7.h5')
-  return model
-model=load_model()
 st.write("""
 # Weather Detection System"""
 )
