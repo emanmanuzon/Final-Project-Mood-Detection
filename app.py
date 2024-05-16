66d418908d423b2b2616423b77661fd5fdef93f0
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 09:37:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ st.write("""
 # Mood Detection"""
 )
 
-webrtc_streamer(key='key', video_processor_factory=VideoTransformer, rtc_configuration=RTConfiguration({"iceServers":[{"urls": ["stun:stun.l.google.com:19302"]}]}))
+webrtc_streamer(key='key', video_processor_factory=VideoTransformer, rtc_configuration=RTCConfiguration({"iceServers":[{"urls": ["stun:stun.l.google.com:19302"]}]}))
