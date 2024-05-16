5325e2835d794786e82b8b5eecaba9ac464bfde5
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 08:22:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import tensorflow as tf
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import cv2
 
 @st.cache_data(experimental_allow_widgets=True)
-
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         return frame
