b5287d9c46ed277ffaddb751d25e5ad28c75138e
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 12:42:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
 def load_model():
-  model=tf.keras.models.load_model('moodmodel2.h5')
+  model=tf.keras.models.load_model('moodmodel.h5')
   return model
 model=load_model()
 st.write("""
